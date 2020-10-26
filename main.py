e064d717005be6e2ad437c9db60d063738b2eebf
Shawn-Chahal <shawn.chahal@gmail.com>
2020-10-26 01:54:43 -0400

    Updated models

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import tensorflow as tf
 import tensorflowjs as tfjs
 
+names = ["connect-four", "reversi"]
 
-model = tf.keras.models.load_model(os.path.join('tf-python', 'model_connect-four.h5'))
-tfjs.converters.save_keras_model(model, os.path.join('tf-js', 'model_connect-four'))
+for name in names:
+    model = tf.keras.models.load_model(os.path.join("tf-python", f"model_{ name }.h5"))
+    tfjs.converters.save_keras_model(model, os.path.join("tf-js", f"model_{ name }"))
