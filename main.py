b1bcacfe8f16ccae58939f70cd5aa2997727a7e4
Shawn-Chahal <shawn.chahal@gmail.com>
2020-11-13 11:55:10 -0500

    Updated Connect Four

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import os
 import tensorflow as tf
 import tensorflowjs as tfjs
 
-names = ["connect-four", "reversi"]
 
 # IMPORTANT: Only convert one model in a single run of the script.
-name = names[0]
+name = "connect-four"
+epoch = "0620"
 
-model = tf.keras.models.load_model(os.path.join("tf-python", f"model_{ name }.h5"))
+model = tf.keras.models.load_model(
+    os.path.join("tf-python", f"model_{ name }_{epoch}.h5")
+)
 tfjs.converters.save_keras_model(model, os.path.join("tf-js", f"model_{ name }"))
