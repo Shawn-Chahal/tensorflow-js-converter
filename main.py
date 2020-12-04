a7aced22185d468a2970f0c9b6ca003ff90bd440
Shawn-Chahal <shawn.chahal@gmail.com>
2020-12-04 16:40:35 -0500

    Added Finish my sentence

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tensorflowjs as tfjs
 
 
 # IMPORTANT: Only convert one model in a single run of the script.
-name = "connect-four_0620"
+name = "finish-my-sentence"
 
 model = tf.keras.models.load_model(os.path.join("tf-python", f"model_{ name }.h5"))
 tfjs.converters.save_keras_model(model, os.path.join("tf-js", f"model_{ name }"))
