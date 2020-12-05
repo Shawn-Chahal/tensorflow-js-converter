963a88b30a639878b00003d4b6bc05fc9fe657e4
Shawn-Chahal <shawn.chahal@gmail.com>
2020-12-04 21:43:00 -0500

    Created GAN model

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tensorflowjs as tfjs
 
 
 # IMPORTANT: Only convert one model in a single run of the script.
-name = "finish-my-sentence"
+name = "gen_model-0359"
 
-model = tf.keras.models.load_model(os.path.join("tf-python", f"model_{ name }.h5"))
-tfjs.converters.save_keras_model(model, os.path.join("tf-js", f"model_{ name }"))
+model = tf.keras.models.load_model(os.path.join("tf-python", f"{ name }.h5"))
+tfjs.converters.save_keras_model(model, os.path.join("tf-js", f"{ name }"))
