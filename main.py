7bfcc80588560920c7932bf20a77d7520f546178
Shawn-Chahal <shawn.chahal@gmail.com>
2020-12-04 23:07:15 -0500

    updated GAN model

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ model = tf.keras.models.load_model(os.path.join("tf-python", f"{ name }.h5"))
 
 if modify == 1:
     model.summary()
-    model = tf.keras.Model(inputs=model.inputs, outputs=model.get_layer("add_3").output)
+    outputs = model.get_layer("add_3").output
+    outputs = tf.keras.layers.Activation('tanh')(outputs)
+    model = tf.keras.Model(inputs=model.inputs, outputs=outputs)
 
 model.summary()
 tfjs.converters.save_keras_model(model, os.path.join("tf-js", f"{ name }"))
