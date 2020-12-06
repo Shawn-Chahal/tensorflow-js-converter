ad2a4b89ce479399140399b3ca70645c3521b894
Shawn-Chahal <shawn.chahal@gmail.com>
2020-12-05 19:11:59 -0500

    Updated finish my sentence

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tensorflowjs as tfjs
 
 
 # IMPORTANT: Only convert one model in a single run of the script.
-name = "model_natural-or-artificial"
+name = "model_finish-my-sentence"
 modify = 0
 
 model = tf.keras.models.load_model(os.path.join("tf-python", f"{ name }.h5"))
@@ -12,7 +12,7 @@ model = tf.keras.models.load_model(os.path.join("tf-python", f"{ name }.h5"))
 if modify == 1:
     model.summary()
     outputs = model.get_layer("add_3").output
-    outputs = tf.keras.layers.Activation('tanh')(outputs)
+    outputs = tf.keras.layers.Activation("tanh")(outputs)
     model = tf.keras.Model(inputs=model.inputs, outputs=outputs)
 
 model.summary()
