f4ed5ae84de1ed394c0846d1ea8534242525b561
Shawn-Chahal <shawn.chahal@gmail.com>
2021-01-11 12:00:44 -0500

    Added Face Generator v1239

--- main.py
+++ main.py
@@ -2,18 +2,8 @@ import os
 import tensorflow as tf
 import tensorflowjs as tfjs
 
-
 # IMPORTANT: Only convert one model in a single run of the script.
-name = "model_finish-my-sentence"
-modify = 0
+name = "gen_model-1239"
 
-model = tf.keras.models.load_model(os.path.join("tf-python", f"{ name }.h5"))
-
-if modify == 1:
-    model.summary()
-    outputs = model.get_layer("add_3").output
-    outputs = tf.keras.layers.Activation("tanh")(outputs)
-    model = tf.keras.Model(inputs=model.inputs, outputs=outputs)
-
-model.summary()
-tfjs.converters.save_keras_model(model, os.path.join("tf-js", f"{ name }"))
+model = tf.keras.models.load_model(os.path.join("tf-python", f"{name}.h5"))
+tfjs.converters.save_keras_model(model, os.path.join("tf-js", f"{name}"))
