cdc4125af9307174660742154994f5c47a5d6997
Shawn Chahal <shawn.chahal@gmail.com>
2022-07-01 18:55:45 -0400

    Updated face generator model

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import tensorflowjs as tfjs
 
 # IMPORTANT: Only convert one model in a single run of the script.
-name = "model_reversi_0313"
+name = "g_model-0205"
 
 model = tf.keras.models.load_model(os.path.join("tf-python", f"{name}.h5"))
 tfjs.converters.save_keras_model(model, os.path.join("tf-js", f"{name}"))
