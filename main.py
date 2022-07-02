9c5d86e797d2a550222363a557e255d5214bae97
Shawn Chahal <shawn.chahal@gmail.com>
2022-07-02 10:32:42 -0400

    Updated natural-or-artificial

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import tensorflowjs as tfjs
 
 # IMPORTANT: Only convert one model in a single run of the script.
-name = "g_model-0205"
+name = "model_natural-or-artificial"
 
 model = tf.keras.models.load_model(os.path.join("tf-python", f"{name}.h5"))
 tfjs.converters.save_keras_model(model, os.path.join("tf-js", f"{name}"))
